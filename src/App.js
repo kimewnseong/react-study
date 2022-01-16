11adbc10a3961e2ddb5fe2022929e6ad0a3dc430
Yoonseong Kim <rladbstjd96@naver.com>
2022-01-16 21:14:03 +0900

    Immer를 사용한 불변성 관리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useMemo } from "react";
 import UserList from "./components/UserList";
 import CreateUser from "./CreateUser";
+import produce from "immer";
 
 const countActiveUsers = (users) => {
   return users.filter((user) => user.active).length;
@@ -38,65 +39,55 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "CHANGE_INPUT": {
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value,
-        },
-      };
+      return produce(state, (draft) => {
+        const input = draft.inputs;
+        input[action.name] = action.value;
+      });
     }
 
     case "CREATE_USER": {
-      return {
-        users: [...state.users, action.user],
-        // input 비워주기
-        inputs: {
-          username: "",
-          email: "",
-        },
-      };
+      return produce(state, (draft) => {
+        const input = draft.inputs;
+
+        draft.users.push(action.user);
+        input.username = "";
+        input.email = "";
+      });
     }
 
     case "TOGGLE_USER": {
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
     }
 
     case "REMOVE_USER": {
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     }
     case "MODIFY_USER": {
-      return {
-        ...state,
-        inputs: {
-          username: action.username,
-          email: action.email,
-          id: action.id,
-        },
-      };
+      return produce(state, (draft) => {
+        const input = draft.inputs;
+        input.username = action.username;
+        input.email = action.email;
+        input.id = action.id;
+      });
     }
 
     case "UPDATE_USER": {
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === state.inputs.id
-            ? {
-                ...user,
-                username: state.inputs.username,
-                email: state.inputs.email,
-              }
-            : user
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === draft.inputs.id);
+        const input = draft.inputs;
+
+        user.username = input.username;
+        user.email = input.email;
+
+        input.username = "";
+        input.email = "";
+      });
     }
 
     default:
